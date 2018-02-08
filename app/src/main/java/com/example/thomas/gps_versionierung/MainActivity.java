28a8b9cd9b2510a59dbe14ab7f364a0bf2871b47
TRAUNER\Thomas <thomastrauner6@gmail.com>
2018-02-08 15:54:16 +0100

    Hey Lukas

--- app/src/main/java/com/example/thomas/gps_versionierung/MainActivity.java
+++ app/src/main/java/com/example/thomas/gps_versionierung/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("thomas du bist der Beste");
+        System.out.println("LoL der Sarkasmus ist greifbar!!!!");
     }
 }
