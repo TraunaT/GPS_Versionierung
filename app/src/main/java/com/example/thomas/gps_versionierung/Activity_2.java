d06669fc459a356aa7568359bf67d89ee9236ca3
lnoemeyer <32090464+lnoemeyer@users.noreply.github.com>
2018-02-12 09:14:50 +0100

    Trauner

---
+++ app/src/main/java/com/example/thomas/gps_versionierung/Activity_2.java
@@ -0,0 +1,8 @@
+package com.example.thomas.gps_versionierung;
+
+/**
+ * Created by lukas on 12.02.2018.
+ */
+
+public class Activity_2 {
+}
--- app/src/main/java/com/example/thomas/gps_versionierung/MainActivity.java
+++ app/src/main/java/com/example/thomas/gps_versionierung/MainActivity.java
@@ -8,8 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println("thomas du bist der Beste");
-        System.out.println("LoL der Sarkasmus ist greifbar!!!!");
+
     }
 }
